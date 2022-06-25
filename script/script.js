43f4dc989eba4ede9b08e81d2f28048b67c470fc
David Gyulinyan <davidgyulinyan@gmail.com>
2022-06-25 21:52:15 +0400

    new updates with animations, during loading and scrolling

--- script/script.js
+++ script/script.js
@@ -1,23 +1,50 @@
-// mobile version menu, burger button
 
+/* mobile version menu, burger button */
 const burgerMenu = document.querySelector('.burger');
-
 const menuIsActive = () => {
     burgerMenu.classList.toggle('active');
 };
-
 burgerMenu.addEventListener('click', menuIsActive);
+/*  */
 
 
-// mobile version menu
-
+/* mobile version menu */
 const mobileMenu = document.querySelector('.mobileMenu');
-
 const menuIsVisible = () => {
     mobileMenu.classList.toggle('activeMenu');
 };
-
 burgerMenu.addEventListener('click', menuIsVisible);
+/*  */
 
 
+/*actvate items when page is loading  */
+const textUnder = document.querySelectorAll('.textUnder');
+document.addEventListener('DOMContentLoaded', () => {
+    textUnder.forEach(item => {
+        item.classList.add("itemsOnImageIsActive")
+    })
+});
+/*  */
+
+
+ /* actvate textPart when page is scrolling */
+const isActive = document.querySelectorAll('.isActive');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > 200) {
+    isActive.forEach(item => {
+        item.classList.add('textPartIsActive');
+    });
+}
+});
+/*  */
+
+    
+ /* actvate space when page is scrolling more than 200 */
+const space = document.querySelector('._space');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > 200) {
+    space.classList.add('_spaceWidth');
+    }
+});
+/*  */
 
